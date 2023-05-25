4f360db53482c6868a6cdb7cbbea2db06c956556
shajicl <shajicl@sheridancollege.ca>
2023-05-25 14:33:28 -0400

    Week3example version1

---
+++ src/week3example/Week3example.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3example;
+
+/**
+ *
+ * @author clind
+ */
+public class Week3example {
+
+    /*PUSH step 1: initalize versioning, .gitconfig file, .gitignore, local repository commit*/
+    public static void main(String[] args) {
+        System.out.println("learning github operations");
+        // TODO code application logic here
+    }
+    
+}
